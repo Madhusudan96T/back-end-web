70a71450b610adb99238e23993116954fd2d69f8
Madhusudan96T <madhusudanxtreme@gmail.com>
2023-07-04 15:21:59 +0530

    after connecting the database

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const express =require('express');
-const app= express();
-app.get("/",(req,resp)=>{
-    resp.send("app is working..")
-}
-);
+const express = require('express');
+const mongoose = require('mongoose');
 
-app.listen(2000)
+const app = express();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/Advocate');
+
+    const productSchema = new mongoose.Schema({});
+
+    const Query = mongoose.model('queries', productSchema);
+    const data = await Query.find();
+    console.log(data);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+};
+
+connectDB();
+
+app.listen(2000, () => {
+  console.log('Server is running on port 2000');
+});
