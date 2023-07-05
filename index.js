b306ee8bf3fd112b12ad2c190d4850dca03958b7
Madhusudan96T <madhusudanxtreme@gmail.com>
2023-07-05 12:48:11 +0530

    after adding the cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors= require('cors');
 require('./db/config.js');
 const userEntity=require('./db/UserEntity.js');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post("/query",async(req,resp)=>{
   let user=new userEntity(req.body);
